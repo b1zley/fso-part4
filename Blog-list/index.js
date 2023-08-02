27979baf300f61ed21c1ca998136c3eb2920c433
b1zley <joshuageardohagan@gmail.com>
2023-08-02 15:12:24 +0100

    implemented logger and middleware utils

--- Blog-list/index.js
+++ Blog-list/index.js
@@ -6,16 +6,32 @@ const mongoose = require('mongoose')
 //import config
 const config = require('./utils/config')
 
+//import middleware util
+const middleware = require('./utils/middleware')
+
 
 app.use(cors())
 app.use(express.json())
+
+//import logger
+const logger = require('./utils/logger')
+
+//use req logger middleware
+app.use(middleware.requestLogger)
 
 //import route controller middleware - must take this middleware into use after json parser
 const blogsRouter = require('./controllers/blogs')
 app.use('/api/blogs', blogsRouter)
 
+
+
+//use unkendpoint middleware
+app.use(middleware.unknownEndpoint)
+
+//use errorhandler middleware
+app.use(middleware.errorHandler)
+
 mongoose.connect(config.mongoUrl)
-const PORT = config.port
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })

e3f2ad7d14fa69e057433eb9a3fe90b479a28322
b1zley <joshuageardohagan@gmail.com>
2023-08-02 15:20:38 +0100

    refactor index -> app.js

---
+++ Blog-list/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+//import config
+const config = require('./utils/config')
+//import middleware util
+const middleware = require('./utils/middleware')
+//import logger
+const logger = require('./utils/logger')
+//import route controller middleware - must take this middleware into use after json parser
+const blogsRouter = require('./controllers/blogs')
+
+
+//use cors
+app.use(cors())
+//use static build from frontend
+app.use(express.static('build'))
+//use json parser
+app.use(express.json())
+//use req logger middleware
+app.use(middleware.requestLogger)
+//use blogsRouter
+app.use('/api/blogs', blogsRouter)
+//use unkendpoint middleware
+app.use(middleware.unknownEndpoint)
+//use errorhandler middleware
+app.use(middleware.errorHandler)
+
+mongoose.connect(config.mongoUrl)
+
+module.exports = app
+
--- Blog-list/index.js
+++ Blog-list/index.js
@@ -1,37 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-//import config
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
-
-//import middleware util
-const middleware = require('./utils/middleware')
-
-
-app.use(cors())
-app.use(express.json())
-
-//import logger
 const logger = require('./utils/logger')
 
-//use req logger middleware
-app.use(middleware.requestLogger)
-
-//import route controller middleware - must take this middleware into use after json parser
-const blogsRouter = require('./controllers/blogs')
-app.use('/api/blogs', blogsRouter)
-
-
-
-//use unkendpoint middleware
-app.use(middleware.unknownEndpoint)
-
-//use errorhandler middleware
-app.use(middleware.errorHandler)
-
-mongoose.connect(config.mongoUrl)
 app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`)
-})
+    logger.info(`Server running on port ${config.port}`)
+  })
